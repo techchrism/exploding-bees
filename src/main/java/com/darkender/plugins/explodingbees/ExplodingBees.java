abb56bfb6e1527413611c27798a95849230f3d9a
TheTechdoodle <admin+github@darkender.com>
2020-06-15 21:29:37 -0700

    add code

---
+++ src/main/java/com/darkender/plugins/explodingbees/ExplodingBees.java
@@ -0,0 +1,26 @@
+package com.darkender.plugins.explodingbees;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExplodingBees extends JavaPlugin implements Listener
+{
+    @Override
+    public void onEnable()
+    {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event)
+    {
+        if(event.getDamager().getType() == EntityType.BEE && event.getEntityType() == EntityType.PLAYER)
+        {
+            event.getDamager().getWorld().createExplosion(event.getDamager().getLocation(), 4.0F);
+            event.getDamager().remove();
+        }
+    }
+}
